21a48dba71543f57e4b306a4e3cae98a00b0f292
Takaya Saito <takaya.saito@outlook.com>
2015-11-12 04:16:56 +0100

    Fix double precision problems with average calculation

--- src/calc_avg_curve.cpp
+++ src/calc_avg_curve.cpp
@@ -2,13 +2,15 @@
 #include <cmath>
 #include <vector>
 #include <string>
+#define EPSILON_D (1.0E-8)
 
 // Prototypes
-void calc_avg_single(const Rcpp::NumericVector& xs,
+void get_yval_single(const Rcpp::NumericVector& xs,
                      const Rcpp::NumericVector& ys,
                      double x_interval,
+                     double x_bins,
                      int vec_size,
-                     std::vector<double>& wk_avg_y);
+                     std::vector<double>& s_y_val);
 
 //
 // Calculate average curves
@@ -35,19 +37,21 @@ Rcpp::List calc_avg_curve(const Rcpp::List& curves,
 
   std::vector<double> tot_y(vec_size, 0.0);    // Total of ys
   std::vector<double> stot_y(vec_size, 0.0);   // Total of squared ys
-  std::vector<double> wk_avg_y(vec_size);      // Average
+  std::vector<double> s_y_val(vec_size, 0.0);  // x values of a single curve
   int n = curves.size();
 
   // Calculate total
   for (int i = 0; i < n; ++i) {
     Rcpp::List c = Rcpp::as<Rcpp::List>(curves[i]);
 
-    calc_avg_single(c["x"], c["y"], x_interval, vec_size, wk_avg_y);
+    get_yval_single(c["x"], c["y"], x_interval, x_bins, vec_size, s_y_val);
 
     for (int j = 0; j < vec_size; ++j) {
-      tot_y[j] += wk_avg_y[j];
-      stot_y[j] += (wk_avg_y[j] * wk_avg_y[j]);
+      tot_y[j] += s_y_val[j];
+      stot_y[j] += (s_y_val[j] * s_y_val[j]);
     }
+    s_y_val.clear();
+    s_y_val.resize(vec_size, 0.0);
   }
 
   // Calculate average & CI
@@ -74,8 +78,6 @@ Rcpp::List calc_avg_curve(const Rcpp::List& curves,
     sd =  ::sqrt(double(n) / double(n - 1)) * ::sqrt(exp2);
     se_y[i] = sd / ::sqrt(double(n));
 
-    se_y[i] = (stot_y[i] / double(n)) - (avg_y[i] * avg_y[i]);
-
     // ci upper bound
     ci_h_y[i] = avg_y[i] + ci_q * se_y[i];
     if (ci_h_y[i] > 1.0) {
@@ -101,39 +103,71 @@ Rcpp::List calc_avg_curve(const Rcpp::List& curves,
   return ret_val;
 }
 
-void calc_avg_single(const Rcpp::NumericVector& xs,
+void get_yval_single(const Rcpp::NumericVector& xs,
                      const Rcpp::NumericVector& ys,
                      double x_interval,
+                     double x_bins,
                      int vec_size,
-                     std::vector<double>& wk_avg_y) {
+                     std::vector<double>& s_y_val) {
 
   std::vector<double> y_tot(vec_size, 0.0); // Total of ys
   std::vector<int> n_y(vec_size, 0);        // Number of each point
+  std::set<double> x_set;
   int idx;
+  double rounded_xval;
 
   // x = 0
-  y_tot[0] = ys[0];
+  s_y_val[0] = ys[0];
   n_y[0]  = 1;
+  idx = 1;
+  while (xs[idx] == 0.0) {
+    s_y_val[1] = ys[idx];
+    n_y[1]  = 1;
+    ++idx;
+  }
+  if (n_y[1] == 0) {
+    s_y_val[1] = s_y_val[0];
+  }
+  n_y[1]  = 1;
 
   // 0 < x < 1
   for (int j = 1; j < xs.size() - 1; ++j) {
-    idx = int((xs[j] / x_interval) + 1.0);
-    y_tot[idx] += ys[j];
+    if (xs[j] == 0.0 || xs[j] == 1.0) {
+      continue;
+    }
+    rounded_xval = ::roundf(xs[j] * x_bins) / x_bins;
+    if (::fabs(rounded_xval - xs[j]) > EPSILON_D) {
+      continue;
+    }
+    idx = int(::roundf(rounded_xval * x_bins) + 1.0);
+    s_y_val[idx] = ys[j];
     ++n_y[idx];
   }
 
   // x = 1
-  y_tot[vec_size - 1] = ys[ys.size() - 1];
+  s_y_val[vec_size - 1] = ys[ys.size() - 1];
   n_y[vec_size - 1]  = 1;
-
-  // Calculate average
-  for (int i = 1; i < y_tot.size(); ++i) {
-    if (n_y[i] == 0) {
-      y_tot[i] = y_tot[i-1];
-      n_y[i] = n_y[i-1];
-    }
-
-    wk_avg_y[i] = y_tot[i] / n_y[i];
+  idx = ys.size() - 2;
+  while (xs[idx] == 1.0) {
+    s_y_val[vec_size - 2] = ys[idx];
+    n_y[vec_size - 2] = 1;
+    --idx;
   }
+  if (n_y[vec_size - 2] == 0) {
+    s_y_val[vec_size - 2] = s_y_val[vec_size - 1];
+  }
+  n_y[vec_size - 2]  = 1;
 
+  //Treat missing values
+  for (int i = 0; i < vec_size; ++i) {
+    if (n_y[i] == 0) {
+      if (n_y[i-1] != 0 && n_y[i+1] != 0) {
+        s_y_val[i] = (s_y_val[i-1] + s_y_val[i+1]) / 2.0;
+      } else if (n_y[i-1] != 0) {
+        s_y_val[i] = s_y_val[i-1];
+      } else if (n_y[i+1] != 0) {
+        s_y_val[i] = s_y_val[i+1];
+      }
+    }
+  }
 }
